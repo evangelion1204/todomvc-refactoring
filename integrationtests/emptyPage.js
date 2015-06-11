f79aeca11706a69916b1ee0e42e958a13618b9ba
Michael Iwersen <mi.iwersen@gmail.com>
2015-06-11 11:20:56 +0200

    First integration tests.

---
+++ integrationtests/emptyPage.js
@@ -0,0 +1,28 @@
+'use strict';
+/* global kommando  require __dirname */
+
+var assertTrimmedString = require('./assertTrimmedString');
+
+var path = require('path');
+var fs = require('fs');
+
+var url = 'http://localhost:8080/index.html';
+var browser = kommando.browser;
+
+browser.manage().window().setSize(800, 600);
+
+describe('the basic site', function () {
+
+  describe('should render correct ', function () {
+    it('when nothing was clicked', function (done) {
+      browser.get(url)
+        .then(function(){return browser.findElement(kommando.webdriver.By.tagName('body')).getInnerHtml(); })
+        .then(function (html) {
+          //fs.writeFileSync(path.join(__dirname, 'golden-master.html'), html);
+          var expected = fs.readFileSync(path.join(__dirname, 'golden-master.html'), 'utf8');
+          assertTrimmedString(html, expected);
+        })
+        .then(done, done);
+    });
+  });
+});
