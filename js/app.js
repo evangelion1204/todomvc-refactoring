4c9f336e19562daa8719e6d10d346100d29877f6
Michael Iwersen <mi.iwersen@gmail.com>
2015-06-11 16:04:35 +0200

    Moved and renamed to keydown.

--- js/app.js
+++ js/app.js
@@ -3,42 +3,41 @@
 var TodoItems = require('./todo-items.js');
 
 var TodoApp = React.createClass({
+  keydown: function (event) {
+    var model1 = this.props.model;
+    var newField = this.refs.newField;
+
+    if (event.which !== 13) {
+      return;
+    } else {
+      event.preventDefault();
+      var val = newField.getDOMNode().value.trim();
+      if (val) {
+        var t = {id: '', title: val, completed: false};
+        var random;
+        for (var i = 0; i < 32; i++) {
+          random = Math.random() * 16 | 0;
+          if (i === 8 || i === 12 || i === 16 || i === 20) {
+            t.id += '-';
+          }
+          t.id += (i === 12 ? 4 : (i === 16 ? (random & 3 | 8) : random)).toString(16);
+        }
+        model1.todos = model1.todos.concat(t);
+        model1.inform();
+
+        newField.getDOMNode().value = '';
+      }
+    }
+  },
 
   render: function() {
     var props = this.props;
     var stats = props.stats;
 
-    var model1 = this.props.model;
-    var newField = this.refs.newField;
-
-    function doun(event) {
-      if (event.which !== 13) {
-        return;
-      } else {
-        event.preventDefault();
-        var val = newField.getDOMNode().value.trim();
-        if (val) {
-          var t = {id: '', title: val, completed: false};
-          var random;
-          for (var i = 0; i < 32; i++) {
-            random = Math.random() * 16 | 0;
-            if (i === 8 || i === 12 || i === 16 || i === 20) {
-              t.id += '-';
-            }
-            t.id += (i === 12 ? 4 : (i === 16 ? (random & 3 | 8) : random)).toString(16);
-          }
-          model1.todos = model1.todos.concat(t);
-          model1.inform();
-
-          newField.getDOMNode().value = '';
-        }
-      }
-    }
-
     return (
       <div>
         <header id="header"><h1>todos</h1>
-          <input ref="newField" id="new-todo" placeholder="What needs to be done?" onKeyDown={doun} autoFocus={true}/>
+          <input ref="newField" id="new-todo" placeholder="What needs to be done?" onKeyDown={this.keydown} autoFocus={true}/>
         </header>
         <TodoItems model={props.model} todos={props.todos} checked={stats.activeCount === 0}/>
         {this.props.stats.activeCount || this.props.stats.completedCount ? this.allDownThere() : (<footer></footer>)}
